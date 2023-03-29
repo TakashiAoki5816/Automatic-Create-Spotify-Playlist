0418636e361813b8075fe22bd9d7fde72342286f
Takashi Aoki <arfaben_5816@icloud.com>
2023-03-30 00:34:18 +0900

    [update]script setup構文の書き方に更新

--- resources/js/components/spotify/CreatePlaylistComponent.vue
+++ resources/js/components/spotify/CreatePlaylistComponent.vue
@@ -1,30 +1,21 @@
+<script setup lang='ts'>
+import axios, { AxiosResponse, AxiosError } from 'axios';
+
+const createPlaylist = (): void => {
+    if (confirm('プレイリストを作成しますか？')) {
+        axios.get('/api/spotify/createPlaylist')
+            .then(function (r: AxiosResponse) {
+                console.log(r);
+            })
+            .catch(function (e: AxiosError) {
+                console.log(e);
+            });
+    }
+};
+</script>
+
 <template>
 	<div>
 		<a href="#" @click="createPlaylist">プレイリストを作成する</a>
 	</div>
 </template>
-
-<script lang='ts'>
-import { defineComponent } from 'vue';
-import axios, { AxiosResponse, AxiosError } from 'axios';
-
-export default defineComponent({
-	setup() {
-		const createPlaylist = (): void => {
-			if (confirm('プレイリストを作成しますか？')) {
-				axios.get('/api/spotify/createPlaylist')
-					.then(function (r: AxiosResponse) {
-						console.log(r);
-					})
-					.catch(function (e: AxiosError) {
-						console.log(e);
-					});
-			}
-		};
-
-		return {
-			createPlaylist,
-		};
-	},
-});
-</script>
